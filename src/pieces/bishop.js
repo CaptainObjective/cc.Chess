2671d37184ce77ce55db17995e44f33d569ef4e3
Michal Nikoniuk <mnikoniuk97@gmail.com>
2019-03-17 22:53:20 +0100

    Goniec - bishop

--- src/pieces/bishop.js
+++ src/pieces/bishop.js
@@ -4,14 +4,31 @@ class Bishop extends Piece {
     constructor(x, y, side) {
         super(x, y, side);
         this.name = 'bishop';
-        this.display = `<i class="fas fa-chess-bishop ${side}"></i>`;//fontawesome bishop
+        this.display = `<i class="fas fa-chess-bishop ${side}"></i>`; //fontawesome bishop
     }
     findLegalMoves() {
 
         const possibleMoves = [];
+        let dx = [-1, -1, 1, 1];
+        let dy = [-1, 1, -1, 1]
+
+        for (let direction = 0; direction < 4; direction++) {
+            for (let counter = 1; counter < 8; counter++) {
+                let newX = this.x + dx[direction] * counter;
+                let newY = this.y + dy[direction] * counter;
+                if (0 <= newX && newX < 8 && 0 <= newY && newY < 8) {
+                    possibleMoves.push(`${newX},${newY}`);
+                }
+            }
+
+        }
 
         return possibleMoves;
     }
+
+
+
 }
+
 
 export default Bishop;
