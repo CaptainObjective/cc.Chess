1e788a40ac25821570fc05d77eb9b513e05f5a21
angelikakuleta <kuleta.an@gmail.com>
2019-03-18 13:10:04 +0100

    add possible moves of knight

--- src/pieces/knight.js
+++ src/pieces/knight.js
@@ -7,8 +7,13 @@ class Knight extends Piece {
         this.display = `<i class="fas fa-chess-knight ${side}"></i>`;//fontawesome knight
     }
     findLegalMoves() {
-
         const possibleMoves = [];
+        for (let i of [-2, -1, 1, 2]) {
+            if (this.x + i >= 0 && this.x + i <= 7 && this.y + (3 - Math.abs(i)) <=7)
+                possibleMoves.push(`${this.x + i},${this.y + (3 - Math.abs(i))}`);
+            if (this.x + i >= 0 && this.x + i <= 7 && this.y - (3 - Math.abs(i)) >= 0)
+                possibleMoves.push(`${this.x + i},${this.y - (3 - Math.abs(i))}`);
+        }
 
         return possibleMoves;
     }
