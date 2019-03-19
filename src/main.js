8919ea59916f9c19769c219719f274f06158552b
Unknown <marcinwos18@gmail.com>
2019-03-19 19:57:43 +0100

    usuwam robaki
    dopisałem metodę do wieży, żeby zwracała elementy tak jak reszta, zrobiłem movement

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import setup from './setup'
 import board from './board';
-import Piece from './pieces/Piece';
+import touched from './touched'
+// import Piece from './pieces/Piece';
 
 //checkWinner();
 
@@ -17,29 +18,47 @@ const getCord = (e) => {
 }
 
 const possibleMoves = (piece) => {
-    console.log(piece.findLegalMoves());
+    // console.log(piece.findLegalMoves());
     for (let posmov of piece.findLegalMoves()) {
-        document.getElementById(posmov).className += 'possibleMove';
+        document.getElementById(posmov).className += ' possibleMove';
+    }
+}
+const moved = (piece, cord) => {
+    piece.move(cord)
+    for (let x = 0; x < board.length; x++) {
+        for (let y = 0; y < board[x].length; y++) {
+            document.getElementById(`${x},${y}`).className = document.getElementById(`${x},${y}`).className.replace(`possibleMove`, '');
+        }
     }
 }
 
 let pieceElement = false;
 
-chessBoard.addEventListener('click', (e) =>{
+chessBoard.addEventListener('click', (e) => {
     let squareCords = getCord(e);
-    //console.log(squareCords);
 
-    if(pieceElement){ //jeśli bierek zaznaczony
+    if (pieceElement) { //jeśli bierek zaznaczony
         if ((squareCords[0] == pieceElement.x) && (squareCords[1] == pieceElement.y)) {
             console.log('To samo pole');
+            return;
         } else {
-            console.log('Ruszam');
+            console.log(squareCords.toString());
+            console.log(pieceElement.findLegalMoves())
+            if (pieceElement.findLegalMoves().includes(squareCords.toString())) {
+                moved(pieceElement, squareCords);
+                pieceElement = false;
+                console.log('Ruszam');
+            } else {
+                console.log('Nie Ruszam');
+                return;
+            }
         }
-
     } else { // jesli bierek niezaznaczony
         if (board[squareCords[0]][squareCords[1]]) { // jesli na polu bierek
             console.log('zaznaczam bierka')
+            // touched(squareCords);
             pieceElement = board[squareCords[0]][squareCords[1]];
+            possibleMoves(pieceElement);
         } else { //jesli na polu brak bierka
             console.log('na tym polu nie ma bierka')
             return;
