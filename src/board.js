85ed8b0f5ba25107720200463819cb9a99d2d690
Unknown <malgorzatatluscik@gmail.com>
2019-03-20 00:12:38 +0100

    pawn -bicie

--- src/board.js
+++ src/board.js
@@ -10,10 +10,6 @@ for (let i = 0; i < 8; i++) {
     board[i] = new Array(8);
 }
 //mamy pustą tablice tu trzeba zaimportować figury wedle przykładu dla pionka
-/*let pawn = new Pawn(6, 0, 'white');
-board[pawn.x][pawn.y] = pawn;
-pawn = new Pawn(6, 1, 'white');
-board[pawn.x][pawn.y] = pawn;*/
 
 let pawn;
 for (let y = 0; y < 8; y++) {
