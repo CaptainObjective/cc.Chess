d32ef47ea894d24cc32a454834ec05ecfd4f9756
Mateusz Pichniarczyk <mat.pich@interia.pl>
2019-03-19 19:40:42 +0100

    new movement

--- src/main.js
+++ src/main.js
@@ -16,32 +16,48 @@ const getCord = (e) => {
     }
 }
 
-const possibleMoves = (piece) => {
+const possibleMoves = (piece, addOrRemove = true) => {
     console.log(piece.findLegalMoves());
     for (let posmov of piece.findLegalMoves()) {
-        document.getElementById(posmov).className += 'possibleMove';
-    }
+        if(addOrRemove){
+            document.getElementById(posmov).className += 'possibleMove'
+        } else {
+            document.getElementById(posmov).className = document.getElementById(posmov).className.replace(`possibleMove`, '');
+        }
+    }   
+}
+
+const isValidMove = (piece, cords) =>{
+    return piece.findLegalMoves().some(ele =>{
+        return ele == `${cords[0]},${cords[1]}`;
+    }); 
 }
 
 let pieceElement = false;
 
 chessBoard.addEventListener('click', (e) =>{
     let squareCords = getCord(e);
-    //console.log(squareCords);
 
     if(pieceElement){ //jeśli bierek zaznaczony
+
+        //jeśli kliknięto to samo pole, na którym stoi bierek to nie pokazuj mozliwosci ruchu i odznacz go
         if ((squareCords[0] == pieceElement.x) && (squareCords[1] == pieceElement.y)) {
-            console.log('To samo pole');
-        } else {
-            console.log('Ruszam');
+            possibleMoves(pieceElement, false);
+        } else { //jesli kliknięto inne pole niż to na którym stoi bierek to sprawdź czy moze się ruszyć i rusz a potem odznacz
+            if(isValidMove(pieceElement, squareCords)){
+                possibleMoves(pieceElement, false);
+                pieceElement.move(`${squareCords[0]},${squareCords[1]}`);
+            } else {
+                possibleMoves(pieceElement, false);
+            }
         }
+        pieceElement = false; //odznacza pionka
 
     } else { // jesli bierek niezaznaczony
-        if (board[squareCords[0]][squareCords[1]]) { // jesli na polu bierek
-            console.log('zaznaczam bierka')
+        if (board[squareCords[0]][squareCords[1]]) { // jesli na polu bierek to go zaznacza i pokazuje mozliwe ruchy
             pieceElement = board[squareCords[0]][squareCords[1]];
-        } else { //jesli na polu brak bierka
-            console.log('na tym polu nie ma bierka')
+            possibleMoves(pieceElement);
+        } else { //jesli na polu brak bierka to nic nie rób
             return;
         }
     }
