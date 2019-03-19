082d8412517b67a82ee7367fcb2fda9ea88b9d01
Michal Nikoniuk <mnikoniuk97@gmail.com>
2019-03-19 19:16:47 +0100

    Revert "Bishop - fixed"
    This reverts commit 3005ae725891fe2b59ade14e19a3ba2383a9579e.

--- src/pieces/bishop.js
+++ src/pieces/bishop.js
@@ -1,5 +1,4 @@
 import Piece from './Piece'
-import board from '../board';
 
 class Bishop extends Piece {
     constructor(x, y, side) {
@@ -8,24 +7,17 @@ class Bishop extends Piece {
         this.display = `<i class="fas fa-chess-bishop ${side}"></i>`; //fontawesome bishop
     }
     findLegalMoves() {
+
         const possibleMoves = [];
-        const dx = [-1, -1, 1, 1];
-        const dy = [-1, 1, -1, 1]
+        let dx = [-1, -1, 1, 1];
+        let dy = [-1, 1, -1, 1]
 
         for (let direction = 0; direction < 4; direction++) {
             for (let counter = 1; counter < 8; counter++) {
-                let newX = parseInt(this.x) + (dx[direction] * counter);
-                let newY = parseInt(this.y) + dy[direction] * counter;
+                let newX = this.x + dx[direction] * counter;
+                let newY = this.y + dy[direction] * counter;
                 if (0 <= newX && newX < 8 && 0 <= newY && newY < 8) {
-                    if (board[newX][newY] != null) {
-                        if (board[newX][newY].side != this.side) {
-                            possibleMoves.push(`${newX},${newY}`);
-                        }
-                    break;
-                    } else {
-                        possibleMoves.push(`${newX},${newY}`);
-
-                    }
+                    possibleMoves.push(`${newX},${newY}`);
                 }
             }
 
