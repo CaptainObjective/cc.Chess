bab868634cae9dbec6960564df2ec4491277ff27
Unknown <malgorzatatluscik@gmail.com>
2019-03-24 13:02:25 +0100

    pawn- fixed bug

--- src/pieces/pawn.js
+++ src/pieces/pawn.js
@@ -14,17 +14,10 @@ class Pawn extends Piece {
     findLegalMoves() {
         //console.log(this.x, this.y);
         const possibleMoves = [];
-<<<<<<< HEAD
-        console.log(possibleMoves);
-
-        if (this.side == 'white') {
-            if(this.x != 0){
-=======
         // console.log(possibleMoves);
 
         if (this.side == 'white') {
             if (this.x != 0) {
->>>>>>> master
                 if (board[this.x - 1][this.y + 1] != null) {
                     if (board[this.x - 1][this.y + 1].side != this.side) {
                         possibleMoves.push(`${this.x - 1},${this.y + 1}`);
@@ -36,38 +29,21 @@ class Pawn extends Piece {
                     }
                 }
             }
-<<<<<<< HEAD
-            if(this.x == 6){              
-=======
             if (this.x == 6) {
->>>>>>> master
                 if (board[this.x - 1][this.y] == null) {
                     possibleMoves.push(`${5},${this.y}`);
                 }
                 if (board[this.x - 1][this.y] == null && board[this.x - 2][this.y] == null) {
-<<<<<<< HEAD
                     possibleMoves.push(`${4},${this.y}`); 
                 }
             }
-            else if (this.x != 0){
+            else if (this.x != 0) {
                 if (board[this.x - 1][this.y] == null) {
                     ((this.x) - 1 >= 0) && possibleMoves.push(`${(this.x) - 1},${this.y}`);
                 } 
             }       
         }  
         
-=======
-                    possibleMoves.push(`${4},${this.y}`);
-                }
-            }
-            else if (this.x != 0) {
-                if (board[this.x - 1][this.y] == null) {
-                    ((this.x) - 1 > 0) && possibleMoves.push(`${(this.x) - 1},${this.y}`);
-                }
-            }
-        }
-
->>>>>>> master
         if (this.side == 'black') {
             if (this.x != 7) {
                 if (board[this.x + 1][this.y - 1] != null) {
@@ -89,21 +65,12 @@ class Pawn extends Piece {
                     possibleMoves.push(`${3},${this.y}`);
                 }
             }
-<<<<<<< HEAD
             else if (this.x != 7){
                 if (board[(this.x) + 1][this.y] == null){
                     ((this.x) + 1 <= 7) && possibleMoves.push(`${(this.x) + 1},${this.y}`);
                 }
             }
             
-=======
-            else if (this.x != 7) {
-                if (board[(this.x) + 1][this.y] == null) {
-                    ((this.x) + 1 > 0) && possibleMoves.push(`${(this.x) + 1},${this.y}`);
-                }
-            }
-
->>>>>>> master
         }
         return possibleMoves;
     }
@@ -111,7 +78,6 @@ class Pawn extends Piece {
         console.log('promocja pionka');
         let newX = parseInt(cord[0]);
         let newY = parseInt(cord[1]);
-<<<<<<< HEAD
 
         let promo = document.createElement('div');
         promo.id = 'promo';
@@ -153,49 +119,6 @@ class Pawn extends Piece {
                             break;
                     }
 
-=======
-
-        let promo = document.createElement('div');
-        promo.id = 'promo';
-
-        promo.style.left = newY * 10 + 'vh';
-        if (side == 'white') {
-            promo.style.top = '10vh';
-        }
-        else {
-            promo.style.bottom = '10vh';        
-        }
-
-        promo.innerHTML = `<div data-piece="queen" class="square"><i class="fas fa-chess-queen ${side}"></i></div>`;
-        promo.innerHTML += `<div data-piece="knight" class="square"><i class="fas fa-chess-knight ${side}"></i></div>`;
-        promo.innerHTML += `<div data-piece="bishop" class="square"><i class="fas fa-chess-bishop ${side}"></i></div>`;
-        promo.innerHTML += `<div data-piece="rook" class="square"><i class="fas fa-chess-rook ${side}" ></i></div>`;
-        
-        document.getElementById('board').appendChild(promo);
-        
-        Array.from(promo.getElementsByTagName('div')).forEach(
-            function (element) {
-                element.addEventListener('click', (e) => {
-                    e.stopPropagation();
-                    let piece = e.currentTarget.dataset.piece;
-                    //console.log(piece);
-                    let promoteTo;
-                    switch (piece) {
-                        case 'queen': 
-                            promoteTo = new Queen(newX, newY, side);
-                            break;
-                        case 'knight':
-                            promoteTo = new Knight(newX, newY, side);
-                            break;
-                        case 'bishop':
-                            promoteTo = new Bishop(newX, newY, side);
-                            break;
-                        case 'rook':
-                            promoteTo = new Rook(newX, newY, side);
-                            break;
-                    }
-
->>>>>>> master
                     board[newX][newY] = promoteTo;
                     document.getElementById(`${newX},${newY}`).innerHTML = promoteTo.display;
                     promo.parentElement.removeChild(promo);
--- src/setup.js
+++ src/setup.js
@@ -1,17 +1,10 @@
-<<<<<<< HEAD
-import board from './board'
-=======
 import board from './board';
 import popup from './settings';
->>>>>>> master
 
 
 const setup = () => {
     console.log(board);
-<<<<<<< HEAD
-=======
     document.getElementById('wrapper').appendChild(popup)
->>>>>>> master
     for (let x = 0; x < board.length; x++) {
         for (let y = 0; y < board[x].length; y++) {
             const square = document.createElement('div');
@@ -20,10 +13,6 @@ const setup = () => {
             square.innerHTML = (board[x][y]) ? board[x][y].display : "";
             square.className = 'square';
             square.className += ((x % 2 == y % 2)) ? ' light' : ' dark';
-<<<<<<< HEAD
-            //square.addEventListener('click', (e) => { touched(e) });
-=======
->>>>>>> master
             document.getElementById('board').appendChild(square);
         }
     }
