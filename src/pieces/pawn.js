37c1e427173088177f0087747d309567f3f80d31
Unknown <marcinwos18@gmail.com>
2019-03-30 21:16:06 +0100

    bug fix

--- src/pieces/pawn.js
+++ src/pieces/pawn.js
@@ -39,7 +39,7 @@ class Pawn extends Piece {
             }
             else if (this.x != 0) {
                 if (board[this.x - 1][this.y] == null) {
-                    ((this.x) - 1 > 0) && possibleMoves.push(`${(this.x) - 1},${this.y}`);
+                    ((this.x) - 1 >= 0) && possibleMoves.push(`${(this.x) - 1},${this.y}`);
                 }
             }
         }
@@ -67,7 +67,7 @@ class Pawn extends Piece {
             }
             else if (this.x != 7) {
                 if (board[(this.x) + 1][this.y] == null) {
-                    ((this.x) + 1 > 0) && possibleMoves.push(`${(this.x) + 1},${this.y}`);
+                    ((this.x) + 1 <= 7) && possibleMoves.push(`${(this.x) + 1},${this.y}`);
                 }
             }
 
@@ -75,7 +75,7 @@ class Pawn extends Piece {
         //console.log(possibleMoves);
         return possibleMoves;
     }
-    promote(cord, side) {      
+    promote(cord, side) {
         console.log('promocja pionka');
         let newX = parseInt(cord[0]);
         let newY = parseInt(cord[1]);
@@ -88,16 +88,16 @@ class Pawn extends Piece {
             promo.style.top = '10vh';
         }
         else {
-            promo.style.bottom = '10vh';        
+            promo.style.bottom = '10vh';
         }
 
         promo.innerHTML = `<div data-piece="queen" class="square"><i class="fas fa-chess-queen ${side}"></i></div>`;
         promo.innerHTML += `<div data-piece="knight" class="square"><i class="fas fa-chess-knight ${side}"></i></div>`;
         promo.innerHTML += `<div data-piece="bishop" class="square"><i class="fas fa-chess-bishop ${side}"></i></div>`;
         promo.innerHTML += `<div data-piece="rook" class="square"><i class="fas fa-chess-rook ${side}" ></i></div>`;
-        
+
         document.getElementById('board').appendChild(promo);
-        
+
         Array.from(promo.getElementsByTagName('div')).forEach(
             function (element) {
                 element.addEventListener('click', (e) => {
@@ -106,7 +106,7 @@ class Pawn extends Piece {
                     //console.log(piece);
                     let promoteTo;
                     switch (piece) {
-                        case 'queen': 
+                        case 'queen':
                             promoteTo = new Queen(newX, newY, side);
                             break;
                         case 'knight':
@@ -130,7 +130,7 @@ class Pawn extends Piece {
     }
 
     enPassant() {
-        
+
     }
 }
 
