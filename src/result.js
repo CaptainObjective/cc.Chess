2bcc44a2495d1954d12dbb51bc0c36931f9b90e1
angelikakuleta <kuleta.an@gmail.com>
2019-03-30 17:38:32 +0100

    add result block

---
+++ src/result.js
@@ -0,0 +1,34 @@
+import setup from './setup';
+import {resetBoard} from './board';
+
+const result = (winner) => {
+    let win;
+
+    const restart = (e) => {
+        e.preventDefault();
+        window.location.reload();
+    }
+
+    const popup = document.createElement('div');
+    popup.className = 'overlay';
+    const res = document.createElement('div');
+    res.className = 'result';
+    const form = document.createElement('form');
+    form.onsubmit = restart;
+
+    form.innerHTML = `
+            <h2>Game over</h2>
+            <p>${winner} is winner</p>
+            `
+    const submit = document.createElement('input');
+    submit.type = 'submit'
+    submit.value = 'Play it again'
+       
+    form.appendChild(submit);
+    res.appendChild(form);
+    popup.appendChild(res);
+
+    document.getElementById('wrapper').appendChild(popup)
+}
+
+export default result;
